97e6f2a5200f33bed249c4a9e5e4414387d9a3da
saurabhvns01 <dlw.saurabhsahani2001@gmail.com>
2024-01-26 15:04:59 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from "./components/Header/Header";
 import About from './components/About/About';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 function App() {
   return (
     <>
+    <SpeedInsights />
     <Header />
     <About />
     </>
