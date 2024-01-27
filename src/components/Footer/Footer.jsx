ab925c7ee1acd31add8c06b0085b6e44f2984431
saurabhvns01 <dlw.saurabhsahani2001@gmail.com>
2024-01-27 10:40:48 +0530

    Footer Modified

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,31 +1,59 @@
 import "./Footer.css";
-import React from 'react'
-import { useState} from 'react'
+import React from "react";
+import { useState } from "react";
+import {  IoHeart, IoLogoReact } from "react-icons/io5";
+
 
 const Footer = () => {
-    const year = new Date().getFullYear();
-    const [activeNav, setActiveNav] = useState('#home');
+  const year = new Date().getFullYear();
+  const [activeNav, setActiveNav] = useState("#home");
   return (
     <section className="footer">
       <div className="container">
         <div className="footer-conatiner">
-          <div className="footer-text">Copyright &copy; <span>{year}</span>. All rights are reserved</div>
+          <div className="footer-text">
+            <span className="text-1">
+              {" "}
+              Copyright &copy; <span>{year}</span>. All rights are reserved
+            </span>
+            <pre className="text-2">Made with <IoHeart className="io-heart"/>  in  <IoLogoReact className="io-react-2"/></pre>
+          </div>
           <div className="footer-links">
-          <a href="#home" onClick={() => setActiveNav('#home')} className={activeNav === '#home' ? 'active': ''}>
-            home
-          </a>
-          <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active': ''}>
-            about
-          </a>
-          <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active': ''}>
-          experience
-          </a>
-          <a href="#projects"  onClick={() => setActiveNav('#projects')} className={activeNav === '#projects' ? 'active': ''}>
-          projects
-          </a>
-          <a href="#contact"  onClick={() => setActiveNav('#contact')}className={activeNav === '#contact' ? 'active': ''}>
-            contact
-          </a>
+            <a
+              href="#home"
+              onClick={() => setActiveNav("#home")}
+              className={activeNav === "#home" ? "active" : ""}
+            >
+              home
+            </a>
+            <a
+              href="#about"
+              onClick={() => setActiveNav("#about")}
+              className={activeNav === "#about" ? "active" : ""}
+            >
+              about
+            </a>
+            <a
+              href="#experience"
+              onClick={() => setActiveNav("#experience")}
+              className={activeNav === "#experience" ? "active" : ""}
+            >
+              experience
+            </a>
+            <a
+              href="#projects"
+              onClick={() => setActiveNav("#projects")}
+              className={activeNav === "#projects" ? "active" : ""}
+            >
+              projects
+            </a>
+            <a
+              href="#contact"
+              onClick={() => setActiveNav("#contact")}
+              className={activeNav === "#contact" ? "active" : ""}
+            >
+              contact
+            </a>
           </div>
         </div>
       </div>
